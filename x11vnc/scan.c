68f3e099a14863138f9d4f4aea2a7cc1761c37c6
Mike <quicklyfrozen@gmail.com>
2016-04-03 02:41:30 -0400

    change how scanning is skipped on os x (remove calls from outer loop(s) instead of fast return from scan display)

--- x11vnc/scan.c
+++ x11vnc/scan.c
@@ -3172,10 +3172,6 @@ static int scan_display(int ystart, int rescan) {
 	int xd_check = 0, xd_freq = 1;
 	static int xd_tck = 0;
 
-    if (macosx_read_cglion && use_xdamage) {
-        return 0;
-    }
-  
 	y = ystart;
 
 	g_now = dnow();
@@ -3364,6 +3360,8 @@ int scan_for_updates(int count_only) {
 	static double last_poll = 0.0;
 	double dtmp = 0.0;
 
+    int skip_scanning = (macosx_read_cglion && use_xdamage);
+  
 	if (unixpw_in_progress) return 0;
  
 	if (slow_fb > 0.0) {
@@ -3435,142 +3433,152 @@ int scan_for_updates(int count_only) {
 		return 0; \
 	}
 
-	/* scan with the initial y to the jitter value from scanlines: */
-	scan_in_progress = 1;
-    tile_count = scan_display(scanlines[scan_count], 0);
-    SCAN_FATAL(tile_count);
+    if (!skip_scanning) {
+      /* scan with the initial y to the jitter value from scanlines: */
+      scan_in_progress = 1;
+      tile_count = scan_display(scanlines[scan_count], 0);
+      SCAN_FATAL(tile_count);
+    }
 
-	/*
-	 * we do the XDAMAGE here too since after scan_display()
-	 * there is a better chance we have received the events from
-	 * the X server (otherwise the DAMAGE events will be processed
-	 * in the *next* call, usually too late and wasteful since
-	 * the unchanged tiles are read in again).
-	 */
-	if (use_xdamage) {
+    /*
+     * we do the XDAMAGE here too since after scan_display()
+     * there is a better chance we have received the events from
+     * the X server (otherwise the DAMAGE events will be processed
+     * in the *next* call, usually too late and wasteful since
+     * the unchanged tiles are read in again).
+     */
+    if (use_xdamage) {
 #ifdef MACOSX
-		if (macosx_console) {
-			;
-		} else 
+        if (macosx_console) {
+            ;
+        } else 
 #endif
-		{
-			if (rawfb_vnc_reflect) {
-				;
-			} else {
-				collect_xdamage(scan_count, 1);
-			}
-		}
-	}
-	if (count_only) {
-		scan_in_progress = 0;
-		fb_copy_in_progress = 0;
-		return tile_count;
-	}
+        {
+            if (rawfb_vnc_reflect) {
+                ;
+            } else {
+                collect_xdamage(scan_count, 1);
+            }
+        }
+    }
+    if (count_only) {
+        scan_in_progress = 0;
+        fb_copy_in_progress = 0;
+        return tile_count;
+    }
 
-	if (xdamage_tile_count) {
-		/* pick up "known" damaged tiles we missed in scan_display() */
-		for (i=0; i < ntiles; i++) {
-			if (tile_has_diff[i]) {
-				continue;
-			}
-			if (tile_has_xdamage_diff[i]) {
-				tile_has_diff[i] = 1;
-				if (tile_has_xdamage_diff[i] == 1) {
-					tile_has_xdamage_diff[i] = 2;
-					tile_count++;
-				}
-			}
-		}
-	}
-	if (dpy && use_xdamage == 1) {
-		static time_t last_xd_check = 0;
-		if (time(NULL) > last_xd_check + 2) {
-			int cp = (scan_count + 3) % NSCAN;
-			xd_do_check = 1;
-			tile_count = scan_display(scanlines[cp], 0);
-			xd_do_check = 0;
-			SCAN_FATAL(tile_count);
-			last_xd_check = time(NULL);
-			if (xd_samples > 200) {
-				static int bad = 0;
-				if (xd_misses > (20 * xd_samples) / 100) {
-					rfbLog("XDAMAGE is not working well... misses: %d/%d\n", xd_misses, xd_samples);
-					rfbLog("Maybe an OpenGL app like Beryl or Compiz is the problem?\n");
-					rfbLog("Use x11vnc -noxdamage or disable the Beryl/Compiz app.\n");
-					rfbLog("To disable this check and warning specify -xdamage twice.\n");
-					if (++bad >= 10) {
-						rfbLog("XDAMAGE appears broken (OpenGL app?), turning it off.\n");
-						use_xdamage = 0;
-						initialize_xdamage();
-						destroy_xdamage_if_needed();
-					}
-				}
-				xd_samples = 0;
-				xd_misses = 0;
-			}
-		}
-	}
+    if (xdamage_tile_count) {
+        /* pick up "known" damaged tiles we missed in scan_display() */
+        for (i=0; i < ntiles; i++) {
+            if (tile_has_diff[i]) {
+                continue;
+            }
+            if (tile_has_xdamage_diff[i]) {
+                tile_has_diff[i] = 1;
+                if (tile_has_xdamage_diff[i] == 1) {
+                    tile_has_xdamage_diff[i] = 2;
+                    tile_count++;
+                }
+            }
+        }
+    }
+  
+    if (!skip_scanning) {
 
-	nap_set(tile_count);
+      if (dpy && use_xdamage == 1) {
+          static time_t last_xd_check = 0;
+          if (time(NULL) > last_xd_check + 2) {
+              int cp = (scan_count + 3) % NSCAN;
+              xd_do_check = 1;
+              tile_count = scan_display(scanlines[cp], 0);
+              xd_do_check = 0;
+              SCAN_FATAL(tile_count);
+              last_xd_check = time(NULL);
+              if (xd_samples > 200) {
+                  static int bad = 0;
+                  if (xd_misses > (20 * xd_samples) / 100) {
+                      rfbLog("XDAMAGE is not working well... misses: %d/%d\n", xd_misses, xd_samples);
+                      rfbLog("Maybe an OpenGL app like Beryl or Compiz is the problem?\n");
+                      rfbLog("Use x11vnc -noxdamage or disable the Beryl/Compiz app.\n");
+                      rfbLog("To disable this check and warning specify -xdamage twice.\n");
+                      if (++bad >= 10) {
+                          rfbLog("XDAMAGE appears broken (OpenGL app?), turning it off.\n");
+                          use_xdamage = 0;
+                          initialize_xdamage();
+                          destroy_xdamage_if_needed();
+                      }
+                  }
+                  xd_samples = 0;
+                  xd_misses = 0;
+              }
+          }
+      }
+    }
 
-	if (fs_factor && frac1 >= fs_frac) {
-		/* make frac1 < fs_frac if fullscreen updates are enabled */
-		frac1 = fs_frac/2.0;
-	}
+    nap_set(tile_count);
 
-	if (tile_count > frac1 * ntiles) {
-		/*
-		 * many tiles have changed, so try a rescan (since it should
-		 * be short compared to the many upcoming copy_tiles() calls)
-		 */
+    if (!skip_scanning) {
+      
+      if (fs_factor && frac1 >= fs_frac) {
+          /* make frac1 < fs_frac if fullscreen updates are enabled */
+          frac1 = fs_frac/2.0;
+      }
 
-		/* this check is done to skip the extra scan_display() call */
-		if (! fs_factor || tile_count <= fs_frac * ntiles) {
-			int cp, tile_count_old = tile_count;
-			
-			/* choose a different y shift for the 2nd scan: */
-			cp = (NSCAN - scan_count) % NSCAN;
+      if (tile_count > frac1 * ntiles) {
+          /*
+           * many tiles have changed, so try a rescan (since it should
+           * be short compared to the many upcoming copy_tiles() calls)
+           */
 
-			tile_count = scan_display(scanlines[cp], 1);
-			SCAN_FATAL(tile_count);
+          /* this check is done to skip the extra scan_display() call */
+          if (! fs_factor || tile_count <= fs_frac * ntiles) {
+              int cp, tile_count_old = tile_count;
+              
+              /* choose a different y shift for the 2nd scan: */
+              cp = (NSCAN - scan_count) % NSCAN;
 
-			if (tile_count >= (1 + frac2) * tile_count_old) {
-				/* on a roll... do a 3rd scan */
-				cp = (NSCAN - scan_count + 7) % NSCAN;
-				tile_count = scan_display(scanlines[cp], 1);
-				SCAN_FATAL(tile_count);
-			}
-		}
-		scan_in_progress = 0;
+              tile_count = scan_display(scanlines[cp], 1);
+              SCAN_FATAL(tile_count);
 
-		/*
-		 * At some number of changed tiles it is better to just
-		 * copy the full screen at once.  I.e. time = c1 + m * r1
-		 * where m is number of tiles, r1 is the copy_tiles()
-		 * time, and c1 is the scan_display() time: for some m
-		 * it crosses the full screen update time.
-		 *
-		 * We try to predict that crossover with the fs_frac
-		 * fudge factor... seems to be about 1/2 the total number
-		 * of tiles.  n.b. this ignores network bandwidth,
-		 * compression time etc...
-		 *
-		 * Use -fs 1.0 to disable on slow links.
-		 */
-		if (fs_factor && tile_count > fs_frac * ntiles) {
-			int cs;
-			fb_copy_in_progress = 1;
-			cs = copy_screen();
-			fb_copy_in_progress = 0;
-			SCAN_FATAL(cs);
-			if (use_threads && pointer_mode != 1) {
-				pointer_event(-1, 0, 0, NULL);
-			}
-			nap_check(tile_count);
-			return tile_count;
-		}
-	}
-	scan_in_progress = 0;
+              if (tile_count >= (1 + frac2) * tile_count_old) {
+                  /* on a roll... do a 3rd scan */
+                  cp = (NSCAN - scan_count + 7) % NSCAN;
+                  tile_count = scan_display(scanlines[cp], 1);
+                  SCAN_FATAL(tile_count);
+              }
+          }
+          scan_in_progress = 0;
+
+          /*
+           * At some number of changed tiles it is better to just
+           * copy the full screen at once.  I.e. time = c1 + m * r1
+           * where m is number of tiles, r1 is the copy_tiles()
+           * time, and c1 is the scan_display() time: for some m
+           * it crosses the full screen update time.
+           *
+           * We try to predict that crossover with the fs_frac
+           * fudge factor... seems to be about 1/2 the total number
+           * of tiles.  n.b. this ignores network bandwidth,
+           * compression time etc...
+           *
+           * Use -fs 1.0 to disable on slow links.
+           */
+          if (fs_factor && tile_count > fs_frac * ntiles) {
+              int cs;
+              fb_copy_in_progress = 1;
+              cs = copy_screen();
+              fb_copy_in_progress = 0;
+              SCAN_FATAL(cs);
+              if (use_threads && pointer_mode != 1) {
+                  pointer_event(-1, 0, 0, NULL);
+              }
+              nap_check(tile_count);
+              return tile_count;
+          }
+      }
+      scan_in_progress = 0;
+
+    } /* skip_scanning */
 
 	/* copy all tiles with differences from display to rfb framebuffer: */
 	fb_copy_in_progress = 1;
@@ -3610,29 +3618,31 @@ dtmp = 0.0;
 if (tile_count) fprintf(stderr, "XX copytile: %.4f  tile_count: %d\n", dnow() - dtmp, tile_count);
 #endif
 
-	/*
-	 * This backward pass for upward and left tiles complements what
-	 * was done in copy_all_tiles() for downward and right tiles.
-	 */
-	tile_diffs = copy_tiles_backward_pass();
-	SCAN_FATAL(tile_diffs);
+    if (!skip_scanning) {
+      /*
+       * This backward pass for upward and left tiles complements what
+       * was done in copy_all_tiles() for downward and right tiles.
+       */
+      tile_diffs = copy_tiles_backward_pass();
+      SCAN_FATAL(tile_diffs);
 
-	if (tile_diffs > frac3 * ntiles) {
-		/*
-		 * we spent a lot of time in those copy_tiles, run
-		 * another scan, maybe more of the screen changed.
-		 */
-		int cp = (NSCAN - scan_count + 13) % NSCAN;
+      if (tile_diffs > frac3 * ntiles) {
+          /*
+           * we spent a lot of time in those copy_tiles, run
+           * another scan, maybe more of the screen changed.
+           */
+          int cp = (NSCAN - scan_count + 13) % NSCAN;
 
-		scan_in_progress = 1;
-		tile_count = scan_display(scanlines[cp], 1);
-		SCAN_FATAL(tile_count);
-		scan_in_progress = 0;
+          scan_in_progress = 1;
+          tile_count = scan_display(scanlines[cp], 1);
+          SCAN_FATAL(tile_count);
+          scan_in_progress = 0;
 
-		tile_diffs = copy_tiles_additional_pass();
-		SCAN_FATAL(tile_diffs);
-	}
-
+          tile_diffs = copy_tiles_additional_pass();
+          SCAN_FATAL(tile_diffs);
+      }
+    }
+  
 	/* Given enough tile diffs, try the islands: */
 	if (grow_fill && tile_diffs > 4) {
 		tile_diffs = grow_islands();
